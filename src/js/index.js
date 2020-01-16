740f95315c105241fd9207d925c698133d3b579a
Christos Ploutarchou <cploutarchou@gmail.com>
2020-01-17 00:42:07 +0200

    Configure Webpack file-loader

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,5 @@
 //Global Application Controller
 import '../css/style.css';
-import x from './test';
-
-console.log(`${x}  module successfully imported!`);
+import '../img/logo.png';
+import '../img/favicon.ico';
+import '../img/icons.svg';
--- webpack.config.js
+++ webpack.config.js
@@ -2,26 +2,39 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/js/index.js',
-  output: {
-    path: path.resolve('./dist'),
-    filename: 'js/bundle.js'
-  },
-  devServer: {
-    contentBase: './dist'
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      filename: 'index.html',
-      template: './src/index.html'
-    })
-  ],
-  module: {
-    rules: [
-      {
-        test: [/\.css$/, /\.svg$/], use: ['style-loader', 'css-loader']
+    entry: './src/js/index.js',
+    output: {
+        path: path.resolve('./dist'),
+        filename: 'js/bundle.js'
+    },
+    devServer: {
+        contentBase: './dist'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: './src/index.html'
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: [/\.css$/], use: ['style-loader', 'css-loader']
 
-      }
-    ]
-  }
+            }, {
+                test: /\.(png|jpe?g|svg|ico|jpg|gif)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: 'images'
+                        }
+                    }
+                ]
+            }]
+    }
 };
+
+
+
