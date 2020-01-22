91459d9a1803a9bedd5ed9e9874cb2d42ed7ef26
Christos Ploutarchou <cploutarchou@gmail.com>
2020-01-22 21:46:19 +0200

    create async function getRecipePuppyResults to be possible fetch data using rapidapi.com/recipe-puppy/api

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,6 @@
 // Global Application Controller
 import Search from "./models/Search";
 
-const search = new Search('pizza');
-search.getResults();
+const search = new Search('https://recipe-puppy.p.rapidapi.com/', 'GET', 'recipe-puppy.p.rapidapi.com', 'ba6857687cmshde324ee2aaaa83bp17b197jsn3e2030e4c0fa');
+search.getRecipePuppyResults('', '', 500);
+
--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -1,30 +1,79 @@
 export default class Search {
 
-    constructor(query,) {
-        this.query = query;
+
+    constructor(url, method, apiHost, apiKey) {
+        // this.query = query;
+        this.url = url;
+        this.method = method;
+        this.apiHost = apiHost;
+        this.apiKey = apiKey;
+    }
+
+    async getRecipePuppyResults(ingredients, query, cnt) {
+
+        if (cnt <= 10) {
+            let page = 1;
+            ingredients = " " ? ingredients = " " : ingredients = `&i=${ingredients}`;
+            query = " " ? query = " " : query = `&q=${query}`;
+            if (!('fetch' in window)) {
+                console.error(`Fetch API Not Supported. Your browser is not up-to-date. Version:  ${navigator.userAgent}`);
+                return;
+            }
+            await fetch(`${this.url}?p=${page}${ingredients}${query}`, {
+                "method": this.method,
+                "headers": {
+                    "x-rapidapi-host": this.apiHost,
+                    "x-rapidapi-key": this.apiKey
+                }
+            })
+                .then(response => {
+                    if (!response.ok) {
+                        throw new Error('Network response was not ok');
+                    }
+                    return response.json();
+                })
+                .then(async json => {
+                    this.results = json.results;
+                    console.log(json.results);
+                }).catch(onerror => {
+                    throw  new Error(`Fetch API Error : ${onerror}`);
+                })
+        } else {
+            let i = cnt / 10;
+            let data = [];
+
+            for (let x = 1; x <= i; x++) {
+                ingredients = " " ? ingredients = " " : ingredients = `&i=${ingredients}`;
+                query = " " ? query = " " : query = `&q=${query}`;
+                if (!('fetch' in window)) {
+                    console.error(`Fetch API Not Supported. Your browser is not up-to-date. Version:  ${navigator.userAgent}`);
+                    return;
+                }
+                await fetch(`${this.url}?p=${x}${ingredients}${query}`, {
+                    "method": this.method,
+                    "headers": {
+                        "x-rapidapi-host": this.apiHost,
+                        "x-rapidapi-key": this.apiKey
+                    }
+                })
+                    .then(response => {
+                        if (!response.ok) {
+                            throw new Error('Network response was not ok');
+                        }
+                        return response.json();
+                    })
+                    .then(async json => {
+                        data.push(json.results);
+                        console.log(data);
+                    }).catch(onerror => {
+                        throw  new Error(`Fetch API Error : ${onerror}`);
+                    })
+
+            }
+
+        }
 
     }
 
-    async getResults() {
-        if (!('fetch' in window)) {
-            console.error(`Fetch API Not Supported. Your browser is not up-to-date. Version:  ${navigator.userAgent}`);
-            return;
-        }
-        await fetch(`https://cors.cpdevlabs.com/http://www.recipepuppy.com/api/?q=${this.query}`)
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('Network response was not ok');
-                }
-                return response.json();
-            })
-            .then(async json => {
-                this.results = json.results;
-                // console.log(json.results);
-            }).catch(onerror => {
-                throw  new Error(`Fetch API Error : ${onerror}`);
-            })
-    };
-
 
 }
-
