be2a04de5b1894728d4cc32585a2477d04acb314
cploutarchou <cploutarchou@gmail.com>
2020-04-01 22:44:31 +0300

    install eslint on project

--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -1,54 +1,54 @@
-import { RECIPES_API, SETTINGS } from "../config";
-import axios from "axios";
+import { RECIPES_API } from '../config'
+import axios from 'axios'
 
 export default class Search {
-  constructor(cnt, query, ingredients) {
+  constructor (cnt, query, ingredients) {
     this.recipePuppy = {
       url: RECIPES_API.RecipePuppy.url,
-      method: "GET",
+      method: 'GET',
       apiHost: RECIPES_API.RecipePuppy.apiHost,
       apiKey: RECIPES_API.RecipePuppy.apiKey
-    };
-    this.cnt = cnt;
-    this.ingredients = `&i=${ingredients}`;
-    this.query = `&q=${query}`;
+    }
+    this.cnt = cnt
+    this.ingredients = `&i=${ingredients}`
+    this.query = `&q=${query}`
   }
 
-  async getResults() {
-    let i = Math.round(this.cnt / 10);
-    console.log(`The i values is ${i}`);
-    let start;
+  async getResults () {
+    let i = Math.round(this.cnt / 10)
+    console.log(`The i values is ${i}`)
+    let start
     if (i === 1) {
-      start = 1;
-      i = 2;
-      console.log(`The i values is ${i}`);
+      start = 1
+      i = 2
+      console.log(`The i values is ${i}`)
     } else {
-      start = 1;
-      i = i + 1;
+      start = 1
+      i = i + 1
     }
-    let data = [];
+    // eslint-disable-next-line prefer-const
+    let data = []
     for (let page = start; page < i; page++) {
-      console.log(i);
       axios({
         method: this.recipePuppy.method,
         url: `${this.recipePuppy.url}?p=${page}${this.ingredients}${this.query}`,
         headers: {
-          "content-type": "application/octet-stream",
-          "x-rapidapi-host": this.recipePuppy.apiHost,
-          "x-rapidapi-key": this.recipePuppy.apiKey
+          'content-type': 'application/octet-stream',
+          'x-rapidapi-host': this.recipePuppy.apiHost,
+          'x-rapidapi-key': this.recipePuppy.apiKey
         }
       })
         .then(response => {
           if (response.status !== 200) {
-            throw new Error("Network response was not ok");
+            throw new Error('Network response was not ok')
           }
-          const newArr = Array.from(response.data.results);
-          data.push(...newArr);
+          const newArr = Array.from(response.data.results)
+          data.push(...newArr)
         })
         .catch(error => {
-          throw new Error(`Fetch API Error : ${error}`);
-        });
+          throw new Error(`Fetch API Error : ${error}`)
+        })
     }
-    return (this.results = data);
+    return (this.results = data)
   }
 }
